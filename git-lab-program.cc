52f0400ea1dcfeea2c942c0084117440760d16ba
Brody Reynolds <br750418@ohio.edu>
2021-08-31 17:21:26 -0400

    Hello Git! Program

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Brody Reynolds>
+ *        Date: <08/31/2021>
+ * Description: Understanding and using GitHub and the git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+
